6b4954142cecab08c24729bd27fd5b345b0c37d3
Anu <anupama.nair3006@gmail.com>
2021-03-26 21:12:01 -0400

    Edited script.js file/ added an array of 4 objects with their characteristics as values
    Edited script.js/ Added an array of 4 pokemons (objects)with their characteristics - name, height and type

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,2 @@
-// To create an alert on the page
-alert('Hello world');
-
-// Adding a variable for my favourite food
-let favoriteFood = 'Burger';
-document.write (favoriteFood);
+// To create a new variable - list of 4 pokemons with their characteristics
+let pokemonList = [{name:"squirtle", height:0.5, type:["water", "dark", "normal"]}, {name:"arbok", height:3.5, type:["poison", "fire", "ice"], {name:"arcanine", height:1.9, type:["fire", "fairy", "steel"]}, {name:"krabby", height:0.4, type:["water", "grass", "electric"]}];
