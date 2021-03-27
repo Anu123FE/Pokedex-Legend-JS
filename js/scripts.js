c3e5746ef07e6886197d892eab89cb84e22c45f2
Anu <anupama.nair3006@gmail.com>
2021-03-26 21:47:36 -0400

    Edited scripts.js file/ cleared a syntax error
    Added a closing curly bracket that was missing at the end of arbok's characteristics

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,2 @@
 // To create a new variable - list of 4 pokemons with their characteristics
-let pokemonList = [{name: "squirtle", height: 0.5, type: ["water", "dark", "normal"]}, {name: "arbok", height: 3.5, type: ["poison", "fire", "ice"], {name: "arcanine", height: 1.9, type: ["fire", "fairy", "steel"]}, {name: "krabby", height: 0.4, type: ["water", "grass", "electric"]}];
+let pokemonList = [{name: "squirtle", height: 0.5, type: ["water", "dark", "normal"]},{name: "arbok", height: 3.5, type: ["poison", "fire", "ice"]},{name: "arcanine", height: 1.9, type: ["fire", "fairy", "steel"]},{name: "krabby", height: 0.4, type: ["water", "grass", "electric"]}];
