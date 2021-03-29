05ec92dcebd59a7ff5b8ea7f117de97f67c9faeb
Anu <anupama.nair3006@gmail.com>
2021-03-29 18:42:47 -0400

    Updated scripts.js file/ added for-loop for listing out names of Pokemons with their height
    Added a for-loop in the scripts.js file to list out the Pokemon names with their heights in DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 // To create a new variable - list of 4 pokemons with their characteristics
-let pokemonList = [{name: "squirtle", height: 0.5, type: ["water", "dark", "normal"]},{name: "arbok", height: 3.5, type: ["poison", "fire", "ice"]},{name: "arcanine", height: 1.9, type: ["fire", "fairy", "steel"]},{name: "krabby", height: 0.4, type: ["water", "grass", "electric"]}];
+let pokemonList = [{name: "Squirtle", height: 0.5, type: ["water", "dark", "normal"]},
+{name: "Arbok", height: 3.5, type: ["poison", "fire", "ice"]},
+{name: "Arcanine", height: 1.9, type: ["fire", "fairy", "steel"]},
+{name: "Krabby", height: 0.4, type: ["water", "grass", "electric"]}];
+
+//adding a for loop to write the Pokemon names in the website's DOM
+for (let i = 0; i < pokemonList.length; i++){
+document.write("<b>" +pokemonList[i].name + "</b>" + " (height: " + pokemonList[i].height +")<br>");
+}
