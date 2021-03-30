e93b7ef52540f43c7bdb915ff691ccd745c0c547
Anu <anupama.nair3006@gmail.com>
2021-03-29 21:42:30 -0400

    Updated Scripts.js file/ added the f condition in the for loop
    Added the if condition in the for loop, to determine the big pokemon, need to work on the format- work in progress.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,14 @@ let pokemonList = [{name: "Squirtle", height: 0.5, type: ["water", "dark", "norm
 {name: "Arcanine", height: 1.9, type: ["fire", "fairy", "steel"]},
 {name: "Krabby", height: 0.4, type: ["water", "grass", "electric"]}];
 
-//adding a for loop to write the Pokemon names in the website's DOM
+//adding a for loop to write the Pokemon names in the website's DOM, also adding html <br> for linebreak after each name and <b></b> to make the names bold
+
+let pokemonHeightThreshold = 3;
 for (let i = 0; i < pokemonList.length; i++){
-document.write("<b>" +pokemonList[i].name + "</b>" + " (height: " + pokemonList[i].height +")<br>");
-}
+  let pokemonString = ("<b>" +pokemonList[i].name + "</b>" + " (height: " + pokemonList[i].height +")<br>");
+
+  if (pokemonList[i].height > pokemonHeightThreshold){
+   document.write("Wow!That's big!");
+  }
+  document.write(pokemonString);
+  }
