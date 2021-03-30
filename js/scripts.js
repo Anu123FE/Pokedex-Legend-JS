47e18526d9f27b9991c498976cac231698355955
Anu <anupama.nair3006@gmail.com>
2021-03-30 19:10:49 -0400

    Updated scripts.js file/ corrected the for loop along with the adding the if condition
    added in comments for the code as well as rectified the for loop and the if-else conditional

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,23 @@ let pokemonList = [{name: "Squirtle", height: 0.5, type: ["water", "dark", "norm
 {name: "Krabby", height: 0.4, type: ["water", "grass", "electric"]}];
 
 //adding a for loop to write the Pokemon names in the website's DOM, also adding html <br> for linebreak after each name and <b></b> to make the names bold
-
+//defining a threshold for the height
 let pokemonHeightThreshold = 3;
+//adding the for loop for printing the names on DOM with the height
 for (let i = 0; i < pokemonList.length; i++){
-  let pokemonString = ("<b>" +pokemonList[i].name + "</b>" + " (height: " + pokemonList[i].height +")<br>");
-
-  if (pokemonList[i].height > pokemonHeightThreshold){
-   document.write("Wow!That's big!");
+  //leaving variable pokemonString undefined initially
+  let pokemonString = "";
+  //setting a condition if height > heightThreshold, defining what should be printed in the pokemonString
+  if (pokemonList[i].height > pokemonHeightThreshold)
+  {
+   pokemonString = "<b>" +pokemonList[i].name + "</b>(height: "
+   + pokemonList[i].height +") - Wow! That is big! <br>";
+  }
+  //adding the else condition, defining what should be printed in the pokemonString if the height < heightThreshold
+  else
+  {
+    pokemonString = "<b>" +pokemonList[i].name + "</b>(height: "
+   + pokemonList[i].height +")<br>";
   }
   document.write(pokemonString);
   }
