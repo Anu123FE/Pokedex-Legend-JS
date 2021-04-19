c8769f2375c751adf45fe7b6cd5d1f6bae528e38
Anu <anupama.nair3006@gmail.com>
2021-04-19 18:37:21 -0400

    Updated the script.js file
    wrapped up the code in IIFE,  added the getAll and the add keys to access the variable inside the IIFE, also added a forEach loop, along with the <hr> tag for a line break and line

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,38 @@
-// To create a new variable - list of 4 pokemons with their characteristics
-let pokemonList = [{name: "Squirtle", height: 0.5, type: ["water", "dark", "normal"]},
-{name: "Arbok", height: 3.5, type: ["poison", "fire", "ice"]},
-{name: "Arcanine", height: 1.9, type: ["fire", "fairy", "steel"]},
-{name: "Krabby", height: 0.4, type: ["water", "grass", "electric"]}];
+//wrapping the code in an IIFE
 
-//adding a for loop to write the Pokemon names in the website's DOM, also adding html <br> for linebreak after each name and <b></b> to make the names bold
-//defining a threshold for the height
-let pokemonHeightThreshold = 3;
-//adding the for loop for printing the names on DOM with the height
-for (let i = 0; i < pokemonList.length; i++){
-  //leaving variable pokemonString undefined initially
-  let pokemonString = "";
-  //setting a condition if height > heightThreshold, defining what should be printed in the pokemonString
-  if (pokemonList[i].height > pokemonHeightThreshold)
-  {
-   pokemonString = "<b>" +pokemonList[i].name + "</b>(height: "
-   + pokemonList[i].height +") - Wow! That is big! <br>";
-  }
-  //adding the else condition, defining what should be printed in the pokemonString if the height < heightThreshold
-  else
-  {
-    pokemonString = "<b>" +pokemonList[i].name + "</b>(height: "
-   + pokemonList[i].height +")<br>";
-  }
-  document.write(pokemonString);
-  }
+let pokemonRepository = (function () {
+
+let pokemonList = [
+{name: "Squirtle", height: 0.5, type: ["water", " dark", " normal"]},
+{name: "Arbok", height: 3.5, type: ["poison", " fire", " ice"]},
+{name: "Arcanine", height: 1.9, type: [" fire", " fairy", " steel"]},
+{name: "Krabby", height: 0.4, type: [" water", " grass", " electric"
+]}
+];
+//adding getAll key, with return statement-will return the pokemonList
+function getAll(){
+  return pokemonList;
+}
+//adding add key, will push a new pokemon with new properties into the list
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
+//adding the return statement
+return {
+  getAll: getAll,
+  add: add,
+  };
+
+})();
+//adding a new pokemon-Charizard to the list
+pokemonRepository.add({name: "Charizard", height: 4.2, type: ["land", "poison", "magic"]});
+//printing to console.log
+console.log(pokemonRepository.getAll());
+console.log(pokemonRepository.add());
+//calling function getAll for accessing the variable in pokemonRepository with a forEach loop and printing in the DOM
+pokemonRepository.getAll().forEach(function(pokemon){
+  document.write(pokemon.name+"<br>");
+  document.write(pokemon.height+"<br>");
+  document.write(pokemon.type+"<br>");
+  document.write("<hr>");
+});
