10590114818f2a1d93a5bc68894d0c9bb5c26378
Anu <anupama.nair3006@gmail.com>
2021-06-05 20:47:35 -0400

    Updated the css, index,html,scripts.js pages
    In the css page- added the css properties for the modal
    Added a new js page for holding the code for modal and then linked it to the DOM in index.html
    Added in the modal code in the scripts.js page
    edited the README page to include the recent updations

--- README.md
+++ README.md
@@ -2,10 +2,15 @@ Project Name: Pokedex-Legend-JS
 
 Project Description: This project gives a list of Pokemons along with their details. The project is expected to provide the user all the details of a particular pokemon by clicking on it.
 
-Clicking on the Pokemon button will now provide the pokemon's details in the alert
+Clicking on the Pokemon button will now provide the pokemon's details in the modal. The modal will display the image, name and the height of the pokemon. The data is derived from external APIs.
+
 
 Tools Used: HTML, CSS, JS
 
 Documentation: APIs:
 https://pokedex.org/
 https://www.codecademy.com/forum_questions/5275f72df10c601d5e0026d2
+
+Added new documentation APIs:
+https://pokeapi.co/api/v2/pokemon/?limit=150
+https://pokeapi.co/api/v2/pokemon/
--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,75 @@
-//removing the other array of pokemons and adding an empty array and adding the external source of pokemons (API)
-let pokemonRepository = (function () {
-  let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-//adding add function-then push feature
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-  //getAll function will return the pokemonList (loaded from API)
-  function getAll() {
-    return pokemonList;
-  }
-  //addListItem is for adding HTML elements and appending the button, also to insert the addEventListener -upon clicking the details of the pokemon will be logged
-  //onto the console log
-  function addListItem(pokemon) {
-    let pokemonList = document.querySelector(".pokemon-list");
-    let listpokemon = document.createElement("li");
-    let button = document.createElement("button");
-    button.innerText = pokemon.name;
-    button.classList.add("button-class");
-    listpokemon.appendChild(button);
-    pokemonList.appendChild(listpokemon);
-    button.addEventListener("click", function(event) {
-      showDetails(pokemon);
-    });
-  }
-//loadList will fetch the data from the API then pass it onto the 1st .then function as a response, if this promise is resolved, a json data format is returned
-//which is then passed onto the 2nd .then function. The json data is stored under results key in the API,so passing a forEach function for each item
-//and then logging in what is needed (item.name and item.url) on the console log. Also the .catch function catches any errors (if the promise is not resolved)
-  function loadList(item) {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
-        console.log(pokemon);
-      });
-    }).catch(function (e) {
-      console.error(e);
-    })
-  }
-//loadDetails function is to determine what details are needed to be fetched just like the loadList function (except for the change in parameter(item))
-  function loadDetails(item) {
-    let url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.types = details.types;
-    }).catch(function (e) {
-      console.error(e);
-    });
-  }
-  function showDetails(item) {
-    pokemonRepository.loadDetails(item).then(function () {
-      console.log(item);
-    });
-  }
+//wrapping the code in an IIFE
 
-  return {
-    add: add,
-    getAll: getAll,
-    addListItem: addListItem,
-    loadList: loadList,
-    loadDetails: loadDetails,
-    showDetails: showDetails
+let pokemonRepository = (function () {
+
+function pokemonListFromExternalSource()
+{
+  const apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let pokemonList = [];
+
+  fetch(apiURL)
+  .then(response=>response.json())
+  .then(response =>
+  {
+    pokemonList = response.results;
+    pokemonList.forEach(pokemon=>{
+    let newPokemonList = document.querySelector(".pokemon-list");
+      let listItemPokemon = document.createElement("li");
+      let button = document.createElement("button");
+      button.innerText = pokemon.name;
+      button.classList.add("button-class");
+      button.addEventListener('click', function() {
+
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        .then(response=>response.json())
+        .then(response=>{
+          console.log(response)
+          console.log(response.sprites.back_shiny)
+
+           let pokemonDetails = `Height: ${response.height}`
+
+
+        modal.showModal(`${pokemon.name}`, `${pokemonDetails}`, `${response.sprites.front_shiny}`);
+
+
+        })
+        .catch(err=>console.error(err))
+
+
+        // alert(pokemonDetails);
+      });
+      listItemPokemon.appendChild(button);
+      newPokemonList.appendChild(listItemPokemon);
+      })
+      })
+      .catch(error=>console.log(error))
+
+}
+
+//adding getAll key, with return statement-will return the pokemonList
+function getAll(){
+  return pokemonList;
+}
+//adding add key, will push a new pokemon with new properties into the list
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
+
+function main()
+{
+  pokemonRepository.pokemonListFromExternalSource()
+}
+
+//adding the return statement
+return {
+  getAll: getAll,
+  add: add,
+  main: main,
+  pokemonListFromExternalSource: pokemonListFromExternalSource
   };
+
 })();
 
 
-pokemonRepository.loadList().then(function () {
-  pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
-  });
-});
+
+pokemonRepository.main();
